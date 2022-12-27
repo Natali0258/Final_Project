6ffec0a89f6ba8270ab57f2c25861fe36741e7ed
natali0258 <natali0258@yandex.ru>
2022-12-27 23:57:32 +0700

    Создала файл с функциями-селекторов

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -8,16 +8,17 @@ import Registration from '../registration/Registration';
 import Authorisation from '../authorization/Authorization';
 import Result from '../result/Result';
 import RegAndAuthResult from '../regAndAuthResult/RegAndAuthResult';
+import * as officersSelectors from '../../storage/selectors';
+import * as authSelectors from '../../storage/selectors';
 import css from './App.module.css';
 import { useSelector } from 'react-redux';
 
 function App() {
-  const isModalActive = useSelector(state => state.officers.isModalActive);
-  const isAuthorization = useSelector(state => state.auth.isAuthorization);
-  const isRegistration = useSelector(state => state.officers.isRegistration);
-  const isError = useSelector(state => state.auth.isError)
-  //const isAuthResult = useSelector(state => state.auth.isAuthResult)
-  //console.log('isError=', isError)
+  const isModalActive = useSelector(officersSelectors.isModalActive);
+  const isRegistration = useSelector(officersSelectors.isRegistration);
+  const isAuthorization = useSelector(authSelectors.isAuthorization);
+  const isError = useSelector(authSelectors.isError);
+
   return (
     <BrowserRouter>
       <div className={css.App}>
@@ -32,11 +33,6 @@ function App() {
               <RegAndAuthResult />
             </Result>
           }
-          {/* {isAuthResult &&
-            <Result>
-              <RegAndAuthResult />
-            </Result>
-          } */}
         </Modal>
       </div >
     </BrowserRouter>
